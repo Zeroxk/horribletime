5addbf56556d1b8d9f57b79c44529b748437918b
bao <chibaosen@gmail.com>
2017-08-08 16:33:21 +0200

    Displaying some box arts.

--- hsub.js
+++ hsub.js
@@ -25,46 +25,99 @@ function makeAjax() {
     return xhttp;
 }
 
-// Get show image from show web page.
-function getShowImage() {
-    var i = 0;
-    for (show of shows) {
-        if (i === 50)
-            break;
-
-        var xhttp = makeAjax();
-        xhttp.onreadystatechange = function() {
-            // Was GET successful?
-            if (this.readyState === 4 && this.status === 200) {
-                var imgs  = this.response.getElementsByClassName('series-image');
-                var anime = document.getElementById('anime');
-
-                [].forEach.call(imgs, function (o) {
-                    anime.appendChild(o.firstChild);
-                })
-            }
-        }
-        xhttp.open("GET",                // Do a HTTP GET.
-                   show.firstChild.href, // Get link to show web page.
-                   true);                // Do it asynchronously!
-        xhttp.send();
-
-        i++;
-    }
-}
-
-// Retrieve the shows from HorribleSubs.info
-function getShows() {
+// Asynchronously get document object from URL and respond.
+function doDoc(url, f) {
     var xhttp = makeAjax();
     xhttp.onreadystatechange = function() {
         // Was GET successful?
         if (this.readyState === 4 && this.status === 200) {
-            shows = this.response.getElementsByClassName('ind-show');
-            getShowImage();
+            f(this.response);
         }
-    };
-    xhttp.open("GET", 'http://horriblesubs.info/shows/', true);
+    }
+    xhttp.open("GET",                // Do a HTTP GET.
+               url,
+               true);                // Do it asynchronously!
     xhttp.send();
+}
+
+// Get document object from URL and respond with self.
+function selfDoDoc(self, url, f) {
+    var xhttp = makeAjax();
+    xhttp.onreadystatechange = function() {
+        // Was GET successful?
+        if (this.readyState === 4 && this.status === 200) {
+            f(self, this.response);
+        }
+    }
+    xhttp.open("GET",                // Do a HTTP GET.
+               url,
+               true);                // Do it asynchronously!
+    xhttp.send();
+}
+
+function openFileDialog() {
+    const {dialog} = require('electron').remote;
+    console.log(dialog.showOpenDialog({properties: ['openFile', 'openDirectory', 'multiSelections']}));
+}
+
+// Clear the <body>-tag in document.
+function emptyBody() {
+    var node = document.body;
+    while (node.firstChild) {
+        node.removeChild(node.firstChild);
+    }
+}
+
+// Anime show.
+class Show {
+    constructor(showDocObj) {
+        this.href = showDocObj.firstChild.href;
+        selfDoDoc(this,            // Provide reference to this object, because the global 'this'-variable gets changed to 'undefined' when entering the response-function.
+                  this.href,
+                  this.setBoxArt);
+    }
+
+    // Set anime show box art image.
+    setBoxArt(self, response) {
+        var imgs         = response.getElementsByClassName('series-image');
+        self.img         = imgs[0].firstChild;
+        self.img.onclick = boxartClick;
+        document.getElementById('anime').appendChild(self.img);
+    }
+}
+
+var showObjects = [];
+
+// Called when user clicks on a box art.
+function boxartClick() {
+    // TODO: Change to new web page.
+    emptyBody();
+    doDoc('', function (response) {
+    });
+}
+
+function asd() {
+    var imgs  = this.response.getElementsByClassName('series-image');
+    var anime = document.getElementById('anime');
+    [].forEach.call(imgs, function (o) {
+        o.firstChild.onclick = boxartClick;
+        anime.appendChild(o.firstChild);
+    })
+}
+
+// Retrieve the shows from HorribleSubs.info
+function getShows() {
+    doDoc('http://horriblesubs.info/shows/', function(response) {
+        shows = response.getElementsByClassName('ind-show');
+        var i = 0;
+        for (s of shows) {
+            if (i === 3)
+                break;
+            showObjects.push(new Show(s));
+           i++;
+        }
+        console.log(showObjects);
+    });
 }
 
 // Filter out shows using the user input written in the input textbox.
